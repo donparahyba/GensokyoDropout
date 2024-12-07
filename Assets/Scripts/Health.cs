a09fd757e5f4313ad1d311be3f4f595e0ac9a81c
donparahyba <andreihudson123@gmail.com>
2024-12-06 21:56:20 -0300

    Primeiro teste com animações

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 100;
+    private int MAX_HEALTH = 100;
+
+    public void Damage(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Não é possível dano negativo");
+        }
+
+        this.health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Não é possível cura negaiva");
+        }
+
+        bool WouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+
+        if (WouldBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else
+        {
+            this.health += amount;
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Você Morreu :/");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ReimuWalks.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class ReimuWalks : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f; // velocidade
+    private Rigidbody2D body; // provem fisica
+    private Vector2 axisMovement; // vetor de duas dimensoes
+    public Animator animator;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>(); // Pega o componente do objeto atrelado ao script
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Atualiza os valores de axisMovement com os inputs do jogador. Input.GetAxisRaw retorna -1, 0 ou 1 dependendo da entrada
+        axisMovement.x = Input.GetAxisRaw("Horizontal");
+        axisMovement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdate é chamado em intervalos constantes, independente da taxa de quadros do jogo. É mais adequado para transformações físicas.
+        Move();
+    }
+
+    private void Move()
+    {
+        // Aplica a movimentação ao Rigidbody2D, normalizando o vetor de movimento para garantir uma velocidade constante em todas as direções.
+        body.linearVelocity = axisMovement.normalized * speed;
+
+        if(axisMovement.x < 0)
+        {
+            animator.SetBool("WalkingLeft", true);
+        }
+        else
+        {
+            animator.SetBool("WalkingLeft", false);
+        }
+
+        if (axisMovement.x > 0)
+        {
+            animator.SetBool("WalkingRight", true);
+        }
+        else
+        {
+            animator.SetBool("WalkingRight", false);
+        }
+    }
+}
